488c691738cbea798d9115202a23c5ef2b03cedc
Mustafa Habibi <mustafa.habibi1988@gmail.com>
2024-05-23 11:52:32 +0200

    functions

---
+++ practice/functions.py
@@ -0,0 +1,14 @@
+def a():
+    print('hello from a')
+a()
+def good():
+    print('my son is nice')
+    
+good()
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
